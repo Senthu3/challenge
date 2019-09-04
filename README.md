18bf610acffd95d59fbb06e4ff0c81be66a52a3f
unknown <uv.visitsenthil@gmail.com>
2019-09-05 00:03:40 +0530

    challenge

---
+++ README.md
@@ -0,0 +1,18 @@
+# gulp-sass-simple-boilerplate
+Boilerplate for simple work
+
+* use sass (scss)
+* use html
+* img minify
+* sass error logging
+* autoprefixer, last 3 browser version
+* nunjucks
+
+## Development
+all works in src folder
+
+## Usage
+Run `git clone https://github.com/Fedorrychkov/gulp-sass-simple-boilerplate`, open folder `cd gulp-sass-simple-boilerplate` and run `npm i`.<br>
+Run `npm start` for start development.
+
+Node Version 10.10.0
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -101,37 +101,43 @@ $(document).ready(function () {
     var imageHeight = $(".item-list ul li img").height();
     $(".item-list ul li .user-details").css("height", imageHeight);
     $(".item-list ul li .user-details").hide();
-});
 
-$('.item-list ul li').on('mouseenter touchstart', function () {
-    var containerWidth = $(".item-list").outerWidth();
-    var currentBlockStart = $(this).position().left;
-    var currentBlockWidth = $(this).outerWidth();
-    var currentBlockTopPosition = $(this).position().top;
-    var currentElement = this;
-    $(".item-list ul li").each(function () {
-        if (this !== currentElement) {
-            $(this).addClass('imageOverlay');
+    $('.item-list ul li').on('mouseenter touchstart', function () {
+        if ($(this).find(".user-details").css('display') == 'block') {
+            return;
+        }
+        var containerWidth = $(".item-list").outerWidth();
+        var currentBlockStart = $(this).position().left;
+        var currentBlockWidth = $(this).outerWidth();
+        var currentBlockTopPosition = $(this).position().top;
+        var currentElement = this;
+        $(".item-list ul li").each(function () {
+            if (this !== currentElement) {
+                $(this).addClass('imageOverlay');
+            }
+        });
+        $(currentElement).find(".user-details").show();
+        $(currentElement).find(".user-details").css({ "left": currentBlockStart, "top": currentBlockTopPosition, "width": currentBlockWidth });
+        if (containerWidth - currentBlockStart > currentBlockWidth) {
+            $(currentElement).next("li").removeClass("imageOverlay");
+            $(currentElement).find(".user-details").animate({ left: currentBlockStart + currentBlockWidth }, "fast");
+        } else {
+            $(currentElement).prev("li").removeClass("imageOverlay");
+            $(currentElement).find(".user-details").animate({ left: currentBlockStart - currentBlockWidth }, "fast");
         }
     });
-    $(currentElement).find(".user-details").show();
-    $(currentElement).find(".user-details").css({ "left": currentBlockStart, "top": currentBlockTopPosition, "width": currentBlockWidth });
-    if (containerWidth - currentBlockStart > currentBlockWidth) {
-        $(currentElement).next("li").removeClass("imageOverlay");
-        $(currentElement).find(".user-details").animate({ left: currentBlockStart + currentBlockWidth }, "fast");
-    } else {
-        $(currentElement).prev("li").removeClass("imageOverlay");
-        $(currentElement).find(".user-details").animate({ left: currentBlockStart - currentBlockWidth }, "fast");
-    }
-});
 
-$('.item-list ul li').on('mouseleave touchend', function () {
-    var that = this;
-    $(that).find(".user-details").hide();
-    $(that).find(".user-details").css("left", "0px");
-    $(that).find(".user-details").css("top", "0px");
-    $(".item-list ul li").each(function () {
-        $(this).removeClass('imageOverlay');
+    $('.item-list ul li').on('mouseleave touchend', function () {
+        if ($(this).find(".user-details").css('display') == 'none') {
+            return;
+        }
+        var that = this;
+        $(that).find(".user-details").hide();
+        $(that).find(".user-details").css("left", "0px");
+        $(that).find(".user-details").css("top", "0px");
+        $(".item-list ul li").each(function () {
+            $(this).removeClass('imageOverlay');
+        });
     });
 });
 
