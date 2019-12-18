3b8bf26375bdc26e57a0e99b58fb12b8a64a6356
Senthil Kumar Mukkandi <uv.visitsenthil@gmail.com>
2019-12-18 23:32:55 +0530

    Add files via upload
    Css Grid Layout

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,43 +1,33 @@
-import * as JQuery from "jquery";
-const $ = JQuery.default;
-// Write Code Here
-$(document).ready(function() {
-    var imageHeight = $( ".item-list ul li img").height();
-    $( ".item-list ul li .user-details").css("height",imageHeight);
-    $( ".item-list ul li .user-details" ).hide();
 
-    $('.item-list ul li').on('mouseenter touchstart', function(){ 
-        
-        var containerWidth = $(".item-list").outerWidth();
-        var currentBlockStart = $(this).position().left;
-        var currentBlockWidth = $(this).outerWidth();
-        var currentBlockHeight = $(this).height();
-        var currentBlockTopPosition = $(this).position().top;
-        var currentElement = this;
-        $( ".item-list ul li" ).each(function() {
-            if(this !== currentElement ){
-                $(this).addClass('imageOverlay');
-            }
-        });
-        $(currentElement).find(".user-details").show();
-        $(currentElement).find(".user-details").css({"left":currentBlockStart,"top":currentBlockTopPosition,"width":currentBlockWidth,"height":currentBlockHeight});
-        if( (containerWidth - currentBlockStart) > currentBlockWidth ){
-            $(currentElement).next("li").removeClass("imageOverlay");
-            $(currentElement).find(".user-details").animate({left:(currentBlockStart + currentBlockWidth)}, "fast");
-        }else{
-            $(currentElement).prev("li").removeClass("imageOverlay");
-            $(currentElement).find(".user-details").animate({left: (currentBlockStart - currentBlockWidth)}, "fast");
-        }
-    });
-    
-    $('.item-list ul li').on('mouseleave touchend', function(){
-        var that = this;
-        $(that).find(".user-details").hide();
-        $(that).find(".user-details").css("left","0px");
-        $(that).find(".user-details").css("top","0px");
-        $( ".item-list ul li" ).each(function() {
-            $(this).removeClass('imageOverlay');
-        });
-    });
+"use strict";
+const headerNav = document.getElementById("js-header--nav");
+const mainWrapper = document.getElementById("js-wrapper");
+// Add Event Listener for the hamburger Menu
+document.getElementById("js-header--toggle").addEventListener("click", displayMobileMenu);
 
-});
+function displayMobileMenu() {
+    headerNav.classList.toggle("header__mobile");
+    mainWrapper.classList.toggle("wrapper__mobile-menu")
+}
+
+let userDetails, parentElement, profileImage, currentTarget, 
+    items = document.getElementsByClassName("js-list--item");
+for (var i = 0; i < items.length; i++) {
+    items[i].addEventListener("mouseenter", toggleUserDetails);
+    items[i].addEventListener("mouseleave", toggleUserDetails);
+}
+function toggleUserDetails(currentElement) {
+    currentTarget = currentElement.target;
+    profileImage = currentTarget.firstElementChild;
+    userDetails = currentTarget.lastElementChild;
+    parentElement = currentTarget.parentElement;
+    if ((parentElement.offsetWidth + parentElement.offsetLeft) == (currentTarget.offsetLeft + currentTarget.offsetWidth)) {
+        userDetails.classList.toggle("last");
+        userDetails.classList.toggle("show");
+        profileImage.classList.toggle("imageOverlay");
+    }
+    else {
+        userDetails.classList.toggle("show");
+        profileImage.classList.toggle("imageOverlay");
+    }
+}
